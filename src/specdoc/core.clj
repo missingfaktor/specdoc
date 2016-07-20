8d5dd50b08c54847ffb31180886c989955ed501c
Rahul Goma Phulore <rahul.phulore.999@gmail.com>
2016-07-21 00:28:11 +0100

    Kick off

---
+++ src/specdoc/core.clj
@@ -0,0 +1,6 @@
+(ns specdoc.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/specdoc/core_test.clj
@@ -0,0 +1,7 @@
+(ns specdoc.core-test
+  (:require [clojure.test :refer :all]
+            [specdoc.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
